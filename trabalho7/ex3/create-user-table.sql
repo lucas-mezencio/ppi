2868b94aaa9be613c2471b88dc678630185350d3
Lucas Mezencio <lucasmezss@hotmail.com>
2022-02-18 18:25:40 -0300

    created ex3 base

---
+++ trabalho7/ex3/create-user-table.sql
@@ -0,0 +1,7 @@
+
+create table user_login
+(
+  id int PRIMARY KEY auto_increment,
+  email char(45) unique,
+  password varchar(255)
+) ENGINE=InnoDB;
---
+++ trabalho7/ex3/registerUser.php
@@ -0,0 +1,56 @@
+<?php
+
+require "../db-connection.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $pdo = mysqlConnect();
+  $email = $_POST["email"] ?? "";
+  $password = $_POST["password"] ?? "";
+  $password = password_hash($password, PASSWORD_DEFAULT);
+
+  try {
+    $sql = <<<SQL
+      insert into user_login (email, password)
+      values (?, ?)
+    SQL;
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$email, $password]);
+
+    header("location: index.html");
+    exit();
+  } catch (Exception $e) {
+    if ($e->errorInfo[1] === 1062) {
+      exit('Usuário já cadastrado: ' . $e->getMessage());
+    } else {
+      exit('Falha ao cadastrar os dados: ' . $e->getMessage());
+    }
+  }
+
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Registrar</title>
+
+    <link rel="stylesheet" href="userForm.css" />
+    <link rel="stylesheet" href="../main.css" />
+  </head>
+  <body>
+    <main>
+      <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <div>
+          <label for="email">Email: </label>
+          <input type="text" id="email" name="email" required />
+        </div>
+        <div>
+          <label for="password">Senha: </label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button>Registrar</button>
+      </form>
+    </main>
+  </body>
+</html>
