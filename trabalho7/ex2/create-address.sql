b4573186ba8ddfae2b61257d931b0c3bacb345d5
Lucas Mezencio <lucasmezss@hotmail.com>
2022-02-18 18:06:23 -0300

    populate db as asked

--- trabalho7/ex2/create-address.sql
+++ trabalho7/ex2/create-address.sql
@@ -6,3 +6,11 @@ create table endereco_ex7
   bairro varchar(50),
   cidade varchar(50),
 ) ENGINE=InnoDB;
+
+insert into endereco_ex7 (cep, rua, bairro, cidade)
+values ("38400-100", "Av João Naves", "Santa Mônica", "Uberlândia"),
+       ("38400-200", "Av Floriano Peixoto", "Centro", "Uberlândia"),
+       ("38400-300", "Av Afonso Pena", "Martins", "Uberlândia"),
+       ("38407-845", "Av Anselmo Alves dos Santos", "Santa Mônica", "Uberlândia"),
+       ("38400-299", "Av Getúlio Vargas", "Centro", "Uberlândia"),
+       ("38400-000", "Av Rondon Pacheco", "Saraiva", "Uberlândia")
