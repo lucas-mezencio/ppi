6fd23430342bd3555bdb98a4d9f3091022efdb46
Lucas Mezencio <lucasmezss@hotmail.com>
2022-03-18 18:05:51 -0300

    create admin account

---
+++ clinica/dbFiles/admin.sql
@@ -0,0 +1,9 @@
+insert into tb_pessoa
+(nome, sexo, email, telefone, cep, logradouro, cidade, estado)
+values
+  ('admin', 'm', 'admin@mail.com', '34912341234', '38400-100', 'rua alabarda', 'berlandia', 'mg');
+
+
+
+insert into tb_funcionario
+values (2, null, '3000.00', '$2y$10$EpTqdx7UzK2Rk0XXV8iNvu.obfi5MdT14Vg/TxQHaQadepsQyMuIu');
